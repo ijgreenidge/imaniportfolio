57a35f4317d82f53527cbebedf5e3b692ad70c84
Imani Greenidge <igreenidge2018@bklawtech.com>
2018-02-04 21:34:29 +0000

    Added Tooltips and icons to footer

--- js/script.js
+++ js/script.js
@@ -1,26 +1,28 @@
  // var config = {
-  //   apiKey: "AIzaSyATiRsiPRE7RDRnVoVeOqpBFWuc7po54SM",
-  //   authDomain: "my-portfolio-cb0ba.firebaseapp.com",
-  //   databaseURL: "https://my-portfolio-cb0ba.firebaseio.com",
-  //   projectId: "my-portfolio-cb0ba",
-  //   storageBucket: "",
-  //   messagingSenderId: "655685115416"
-  // };
-  // firebase.initializeApp(config);
-  
-  
-  // var firstName = firebase.database().ref("info.first-name");
-  
-  // firstName.on('value', function(snapshot){
-  //   console.log(snapshot.val()); 
-  //   $(".name").html(snapshot.val());
-  // });
-  
-  
-   $(document).ready(function(){
-      $('.parallax').parallax();
-    });
-    
+ //   apiKey: "AIzaSyATiRsiPRE7RDRnVoVeOqpBFWuc7po54SM",
+ //   authDomain: "my-portfolio-cb0ba.firebaseapp.com",
+ //   databaseURL: "https://my-portfolio-cb0ba.firebaseio.com",
+ //   projectId: "my-portfolio-cb0ba",
+ //   storageBucket: "",
+ //   messagingSenderId: "655685115416"
+ // };
+ // firebase.initializeApp(config);
 
-  
-  
+
+ // var firstName = firebase.database().ref("info.first-name");
+
+ // firstName.on('value', function(snapshot){
+ //   console.log(snapshot.val()); 
+ //   $(".name").html(snapshot.val());
+ // });
+
+
+ $(document).ready(function() {
+  $('.parallax').parallax();
+
+ });
+
+ $(document).ready(function() {
+  $('.tooltipped').tooltip({ delay: 50 });
+ });
+ 
